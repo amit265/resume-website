259a8b367c2e7105d709c9cb54b61b57608a0595
Amit kumar <amitk.kumar414@gmail.com>
2025-07-07 14:47:48 +0530

    resume fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -119,7 +119,7 @@ function Header() {
                 className="w-24 ml-auto mr-4 mt-2 cursor-pointer px-4 py-2 rounded-md bg-[#ea5147] hover:bg-[#fefffa] hover:text-[#323954]"
               >
                 <a
-                  href="/amit-resume.pdf"
+                  href="/assets/amit-resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
